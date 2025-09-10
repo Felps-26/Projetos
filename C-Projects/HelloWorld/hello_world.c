0001e79c42bdc53299c5f2b4b9bd0b6e44460d1e
Felps_ <filipiramos4152@gmail.com>
2025-09-10 20:54:10 -0300

    Adicionar programa Hello World em CCreate hello_world.c

---
+++ C-Projects/HelloWorld/hello_world.c
@@ -0,0 +1,17 @@
+/*
+ * Hello World Program em C
+ * Programa básico de introdução à linguagem C
+ * 
+ * Autor: Felps-26
+ * Data: Setembro 2025
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Olá, Mundo!\n");
+    printf("Este é o meu primeiro programa em C!\n");
+    printf("Bem-vindo ao repositório de projetos em C!\n");
+    
+    return 0;
+}
